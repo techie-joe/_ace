1ee3c537112765aad6808dcdaab573e585b73e19
Techie Joe <185759893+techie-joe@users.noreply.github.com>
2024-12-16 22:06:58 +0800

    update all

--- gulpfile.js
+++ gulpfile.js
@@ -1,31 +1,71 @@
 const _src = {
   root: "_ace",
-  html: "pages",
-  php: "pages",
-  js: ["scripts/gjs_/**/*.js"],
+  html: ["pages/**/*.html.pug"],
+  php : ["pages/**/*.php.pug"],
+  js  : ["scripts/gjs_/**/*.js"],
   scss: ["styles/scss/**/*.scss"]
 };
+
 const _dest = {
   root: "../ace",
-  css: "../ace/assets/css",
-  js: "scripts/gjs"
+  css : "../ace/assets/css",
+  js  : "scripts/gjs"
 };
+
 const _gulp = {
   js : "gulp.js"
 }
 
 const { watch, series, parallel, src, dest } = require('gulp');
 const rename = require('gulp-rename');
+const pug = require("gulp-pug");
+const watchOpt = { ignoreInitial: false };
 
 function clean(callback) {
   callback(); // place code for your task here
 }
 exports.clean = clean;
 
-function css(callback) {
-  callback(); // place code for your task here
+function html() {
+  return src(_src.html)
+    .pipe(pug({ pretty: false }))
+    .pipe(rename(function (path) {
+      path.basename = path.basename.substring(0, path.basename.lastIndexOf('.'));
+    }))
+    .pipe(dest(_dest.root));
+}
+function php() {
+  return src(_src.php)
+    .pipe(pug({ pretty: false }))
+    .pipe(rename(function (path) {
+      path.basename = path.basename.substring(0, path.basename.lastIndexOf('.'));
+      path.extname = '.php';
+    }))
+    .pipe(dest(_dest.root));
+}
+function pagesw() {
+  watch(_src.html, watchOpt, html);
+  watch(_src.php, watchOpt, php);
+}
+
+exports.pages = parallel(html, php);
+exports.pagesw = pagesw;
+
+// sass
+// : task to take scss files from _src to _dest
+// : outputStyle: compressed | expanded
+const sass = require("gulp-sass")(require("sass"));
+function css() {
+  return src(_src.scss)
+    .pipe(sass({ outputStyle: 'compressed' })
+    .on("error", sass.logError))
+    .pipe(dest(_dest.css));
+}
+function cssw() {
+  watch(_src.scss, watchOpt, css);
 }
 exports.css = css;
+exports.cssw = cssw;
 
 function js() {
   return src(_src.js)
@@ -41,13 +81,12 @@ exports.build = build;
 
 exports.default = series(
   clean,
-  parallel( css, js ),
+  parallel( html, php, css, js ),
   build
 );
 
-const watchOpt = { ignoreInitial: false };
-
-exports.watch = function() {
-  watch(_src.scss, watchOpt, css);
+exports.watch = function () {
+  pagesw();
+  cssw();
   watch(_src.js, watchOpt, js);
 };
