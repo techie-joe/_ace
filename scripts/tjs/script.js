79b44e2b9737a8be4c1e1b5eb33714dc2b215326
Techie Joe <185759893+techie-joe@users.noreply.github.com>
2024-12-17 10:43:30 +0800

    update webpack js

--- scripts/tjs/script.js
+++ scripts/tjs/script.js
@@ -1,2 +1,2 @@
+/*! Techie Joe's Base v0.1.0 | Copyright 2024 Techie Joe | https://github.com/techie-joe/base */
 "use strict";
-/* Techie Joe's Base v0.1.0 | Copyright 2024 Techie Joe | https://github.com/techie-joe/base */ 
